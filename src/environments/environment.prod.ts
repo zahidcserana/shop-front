7023fa6418e229b3848dc890cdfde57e5eedbaec
Zahid <zahid1004048@gmail.com>
2025-07-05 13:57:28 +0600

    Change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,6 @@
 export const environment = {
   production: true,
-  // api_url: 'https://afcpharmacycumillaapi.cerebrum.host/',
-  api_url: 'http://localhost/@analyticalJ/Showroom/shop-back/',
-  // api_url: 'http://192.168.0.130/spe_api/',
+  api_url: 'https://analyticalj.com/client/pfashion/',
   protocal: "http://",
   storeHoshName: "localhost:4200",
   patnerHoshName: "localhost:4200",
