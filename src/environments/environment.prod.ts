d71e5cd0135b2a2dc0ef771d3a30cc98665386dc
Zahid <zahid1004048@gmail.com>
2025-07-09 16:52:32 +0600

    Remove environments folder from Git tracking

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  api_url: 'https://analyticalj.com/client/pfashion/',
-  protocal: "http://",
-  storeHoshName: "localhost:4200",
-  patnerHoshName: "localhost:4200",
-  partner: "localhost"
-};
